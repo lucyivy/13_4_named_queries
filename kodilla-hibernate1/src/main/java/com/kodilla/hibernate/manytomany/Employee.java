d8254233303184635521c266c3679206de62810a
Lucyna.Bluszcz <lucynabluszcz@gmail.com>
2018-05-02 21:22:07 +0200

    13_4_company

--- kodilla-hibernate1/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
+++ kodilla-hibernate1/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
@@ -5,20 +5,25 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedQuery(
+        name = "Employee.retrieveEmployeeNamed",
+        query = "FROM Employee WHERE lastName = :LAST_NAME"
+)
+
 @Entity
 @Table(name = "EMPLOYEES")
 public class Employee {
     private int id;
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private List<Company> companies = new ArrayList<>();
 
     public Employee() {
     }
 
     public Employee(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+        this.firstName = firstname;
+        this.lastName = lastname;
     }
 
     @Id
@@ -31,14 +36,14 @@ public class Employee {
 
     @NotNull
     @Column(name = "FIRSTNAME")
-    public String getFirstname() {
-        return firstname;
+    public String getFirstName() {
+        return firstName;
     }
 
     @NotNull
     @Column(name = "LASTNAME")
-    public String getLastname() {
-        return lastname;
+    public String getLastName() {
+        return lastName;
     }
 
     @ManyToMany(cascade = CascadeType.ALL)
@@ -56,12 +61,12 @@ public class Employee {
         this.id = id;
     }
 
-    private void setFirstname(String firstname) {
-        this.firstname = firstname;
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    private void setLastname(String lastname) {
-        this.lastname = lastname;
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     private void setCompanies(List<Company> companies) {
--- kodilla-hibernate1/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
+++ kodilla-hibernate1/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
@@ -1,19 +1,19 @@
 package com.kodilla.hibernate.manytomany.dao;
 
-import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.NamedNativeQuery;
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
 @Repository
-interface CompanyDao extends CrudRepository<Company, Integer> {
+interface EmployeeDao extends CrudRepository<Employee, Integer> {
 
-    @Query(nativeQuery = true)
-    List<Company> retrieveNamesWithLetters(@Param("LETTERS") String name);
+    @Query
+    List<Employee> retrieveEmployeeNamed(@Param("LAST_NAME") String lastName);
+
 }
